afc412323991f5747d816d8773e731af91380946
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-10-29 19:12:39 -0400

    Setup file

---
+++ sub_strings.rb
@@ -0,0 +1,10 @@
+
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substring (string, dictionary)
+  string.split
+end
+
+substrings("below", dictionary)
+#=> { "below" => 1, "low" => 1 }
