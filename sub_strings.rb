5f997eba8bd0a6d54204ebcd3de5fc2de527139b
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-10-30 10:28:50 -0400

    Clarify inputs

--- sub_strings.rb
+++ sub_strings.rb
@@ -9,10 +9,10 @@ def substrings (input, dictionary)
   input_array = cleanInput(input).split(' ')
   matching_hash = Hash.new(0)
   input_array.each do |input_word|
-    dictionary.each do |word|
-      result = word.split('') - cleanInput(input_word).split('')
+    dictionary.each do |dictionary_word|
+      result = dictionary_word.split('') - cleanInput(input_word).split('')
       if result.empty?
-        matching_hash[word] += 1
+        matching_hash[dictionary_word] += 1
       end
     end
   end
